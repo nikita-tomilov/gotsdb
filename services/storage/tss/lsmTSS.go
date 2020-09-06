3e50f34cd799cff38b7ef0ec17d833f1ab1f6486
Nikita Tomilov <programmer174@icloud.com>
2020-09-06 18:58:37 +0300

    lsmTSS: use datasources info

--- services/storage/tss/lsmTSS.go
+++ services/storage/tss/lsmTSS.go
@@ -6,27 +6,53 @@ import (
 	"github.com/nikita-tomilov/golsm/dto"
 	"github.com/nikita-tomilov/gotsdb/proto"
 	"github.com/nikita-tomilov/gotsdb/utils"
+	"sync"
 	"time"
 )
+
+type StoragesForDatasource struct {
+	storageReader *golsm.StorageReader
+	storageWriter *golsm.StorageWriter
+}
 
 type LSMTSS struct {
 	Path                        string `summer.property:"ts.filePath|/tmp/gotsdb/tss"`
 	CommitlogFlushPeriodSeconds int    `summer.property:"tslsm.commitlogFlushPeriodSeconds|10"`
 	MemtExpirationPeriodSeconds int    `summer.property:"tslsm.memtExpirationPeriodSeconds|10"`
-	storageReader               *golsm.StorageReader
-	storageWriter               *golsm.StorageWriter
+	forDatasource               map[string]StoragesForDatasource
+	mutex                       *sync.Mutex
 }
 
-//TODO: WE DO IGNORE DATASOURCES FOR NOW HERE, PLEASE NOTE THAT!
+func (lsm *LSMTSS) getOrInitStorage(datasource string) StoragesForDatasource {
+	val, ok := lsm.forDatasource[datasource]
+	if ok {
+		return val
+	}
 
-func (lsm *LSMTSS) InitStorage() {
-	lsm.storageReader, lsm.storageWriter = golsm.InitStorage(
-		lsm.Path+"/commitlog",
+	lsm.mutex.Lock()
+	val, ok = lsm.forDatasource[datasource]
+	if ok {
+		lsm.mutex.Unlock()
+		return val
+	}
+
+	rootPath := lsm.Path + "/" + datasource
+	storageReader, storageWriter := golsm.InitStorage(
+		rootPath+"/commitlog",
 		10,
 		time.Duration(lsm.CommitlogFlushPeriodSeconds)*time.Second,
 		time.Duration(lsm.MemtExpirationPeriodSeconds)*time.Second,
-		lsm.Path+"/sst",
+		rootPath+"/sst",
 		100)
+	lsm.forDatasource[datasource] = StoragesForDatasource{storageReader: storageReader, storageWriter: storageWriter}
+
+	lsm.mutex.Unlock()
+	return lsm.forDatasource[datasource]
+}
+
+func (lsm *LSMTSS) InitStorage() {
+	lsm.mutex = &sync.Mutex{}
+	lsm.forDatasource = make(map[string]StoragesForDatasource)
 }
 
 func (lsm *LSMTSS) CloseStorage() {
@@ -39,11 +65,11 @@ func (lsm *LSMTSS) Save(dataSource string, data map[string]*proto.TSPoints, expi
 	for k, v := range data {
 		converted[k] = convertTSPtoMeasurement(v)
 	}
-	lsm.storageWriter.Store(converted, expireAt)
+	lsm.getOrInitStorage(dataSource).storageWriter.Store(converted, expireAt)
 }
 
 func (lsm *LSMTSS) Retrieve(dataSource string, tags []string, fromTimestamp uint64, toTimestamp uint64) map[string]*proto.TSPoints {
-	retrieved := lsm.storageReader.Retrieve(tags, fromTimestamp, toTimestamp)
+	retrieved := lsm.getOrInitStorage(dataSource).storageReader.Retrieve(tags, fromTimestamp, toTimestamp)
 	deconverted := make(map[string]*proto.TSPoints)
 	for tag, values := range retrieved {
 		deconverted[tag] = convertMeasurementToTSP(values)
@@ -70,8 +96,7 @@ func convertMeasurementToTSP(measurements []dto.Measurement) *proto.TSPoints {
 }
 
 func (lsm *LSMTSS) Availability(dataSource string, fromTimestamp uint64, toTimestamp uint64) []*proto.TSAvailabilityChunk {
-	//TODO: fix
-	from, to := lsm.storageReader.Availability()
+	from, to := lsm.getOrInitStorage(dataSource).storageReader.Availability()
 	if (from == 0) && (to == 0) {
 		return []*proto.TSAvailabilityChunk{}
 	}
